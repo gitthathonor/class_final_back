db8ed1dafd582502432d94a95e33e2b959e7291d
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-27 04:32:30 +0900

    initial-commit

---
+++ src/main/java/site/hobbyup/class_final_back/ClassFinalBackApplication.java
@@ -0,0 +1,13 @@
+package site.hobbyup.class_final_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassFinalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassFinalBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/hobbyup/class_final_back/ClassFinalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package site.hobbyup.class_final_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassFinalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
