f41c39ad81fd1d0256d02b639156c1f3a6cfe2ec
hyonna12 <hyonna01@gmail.com>
2022-11-29 13:57:12 +0900

    프로필 등록 완료 - 이름 랜덤지정

--- src/main/java/site/hobbyup/class_final_back/service/ProfileService.java
+++ src/main/java/site/hobbyup/class_final_back/service/ProfileService.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.imageio.ImageIO;
 
@@ -51,8 +52,11 @@ public class ProfileService {
         byte[] decodeByte = Base64.decodeBase64(encodeFile);
         log.debug("디버그 : 디코딩2" + decodeByte);
 
+        // 이미지 이름
+        String fileName = UUID.randomUUID().toString();
+        String filePath = "C:\\Temp\\upload\\" + fileName + ".jpg";
+
         // 이미지 저장
-        String filePath = "C:\\Temp\\upload\\" + decodeByte + ".jpg";
         fos = new FileOutputStream(filePath); // 현위치에 path명으로 파일생성
         fos.write(decodeByte); // 파일에 buffer의 모든 내용 출력
         fos.close();
