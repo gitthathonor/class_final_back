ac2dd34377febfc0d0535ef296a1c7e00f772659
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-29 18:22:35 +0900

    lesson 리포지토리 생성

--- src/main/java/site/hobbyup/class_final_back/domain/lesson/LessonRepository.java
+++ src/main/java/site/hobbyup/class_final_back/domain/lesson/LessonRepository.java
@@ -1,5 +1,7 @@
 package site.hobbyup.class_final_back.domain.lesson;
 
-public class LessonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
 
 }
