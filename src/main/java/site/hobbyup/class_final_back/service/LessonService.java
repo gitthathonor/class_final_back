e370df872c6a7ef5cd3b6fca626ed5da56c4ffed
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-30 18:04:41 +0900

    클래스 등록 서비스 작성중

--- src/main/java/site/hobbyup/class_final_back/service/LessonService.java
+++ src/main/java/site/hobbyup/class_final_back/service/LessonService.java
@@ -1,5 +1,6 @@
 package site.hobbyup.class_final_back.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
