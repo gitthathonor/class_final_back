549564de80ff6b4471452c6f45c21ab5e2fea186
gitthathonor <jeong.suyoung92@gmail.com>
2022-12-01 20:41:05 +0900

    review 작성하기 컨트롤러, 서비스, dto 작성완료

--- src/main/java/site/hobbyup/class_final_back/web/ReviewApiController.java
+++ src/main/java/site/hobbyup/class_final_back/web/ReviewApiController.java
@@ -1,5 +1,28 @@
 package site.hobbyup.class_final_back.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.hobbyup.class_final_back.dto.ResponseDto;
+import site.hobbyup.class_final_back.dto.review.ReviewReqDto.ReviewSaveReqDto;
+import site.hobbyup.class_final_back.dto.review.ReviewRespDto.ReviewSaveRespDto;
+import site.hobbyup.class_final_back.service.ReviewService;
+
+@RequiredArgsConstructor
+@RestController
 public class ReviewApiController {
 
+  private final ReviewService reviewService;
+
+  @PostMapping("/api/user/{userId}/lesson/{lessonId}/review")
+  public ResponseEntity<?> saverReview(@RequestBody ReviewSaveReqDto reviewSaveReqDto,
+      @PathVariable("userId") Long userId, @PathVariable("lessonId") Long lessonId) {
+    ReviewSaveRespDto reviewSaveRespDto = reviewService.saveReview(reviewSaveReqDto, userId, lessonId);
+    return new ResponseEntity<>(new ResponseDto<>("리뷰작성성공", reviewSaveRespDto), HttpStatus.CREATED);
+  }
 }
