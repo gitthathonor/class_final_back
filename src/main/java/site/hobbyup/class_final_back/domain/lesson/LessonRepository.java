d83dc927bfd4034c839bc20647c2311205b3ff37
gitthathonor <jeong.suyoung92@gmail.com>
2022-12-01 19:32:18 +0900

    lesson 리스트 보기 일시 중지

--- src/main/java/site/hobbyup/class_final_back/domain/lesson/LessonRepository.java
+++ src/main/java/site/hobbyup/class_final_back/domain/lesson/LessonRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface LessonRepository extends JpaRepository<Lesson, Long>, Dao {
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
 
   @Query("select l from Lesson l join fetch l.category c where l.category.id = :categoryId")
   List<Lesson> findByCategory(@Param("categoryId") Long categoryId);
