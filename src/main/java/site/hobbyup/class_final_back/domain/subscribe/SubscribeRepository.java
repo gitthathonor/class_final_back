68014182b73c3c44c80c06478b6acdbeada380c4
hyonna12 <hyonna01@gmail.com>
2022-12-01 11:59:07 +0900

    찜 테이블 생성

---
+++ src/main/java/site/hobbyup/class_final_back/domain/subscribe/SubscribeRepository.java
@@ -0,0 +1,6 @@
+package site.hobbyup.class_final_back.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+}
