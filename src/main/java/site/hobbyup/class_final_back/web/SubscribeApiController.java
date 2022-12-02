47b6cce5bf7f7d7fcadd731f62fc9b4ebccf1a71
hyonna12 <hyonna01@gmail.com>
2022-12-02 15:52:47 +0900

    구독 리스트보기 - review 테이블 추가해야함

--- src/main/java/site/hobbyup/class_final_back/web/SubscribeApiController.java
+++ src/main/java/site/hobbyup/class_final_back/web/SubscribeApiController.java
@@ -6,21 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import site.hobbyup.class_final_back.config.auth.LoginUser;
-import site.hobbyup.class_final_back.domain.lesson.Lesson;
-import site.hobbyup.class_final_back.domain.subscribe.Subscribe;
-import site.hobbyup.class_final_back.domain.user.User;
+import site.hobbyup.class_final_back.config.exception.CustomApiException;
 import site.hobbyup.class_final_back.dto.ResponseDto;
 import site.hobbyup.class_final_back.dto.subscribe.SubscribeReqDto.SubscribeSaveReqDto;
-import site.hobbyup.class_final_back.dto.subscribe.SubscribeRespDto.SubscribeDeleteRespDto;
+import site.hobbyup.class_final_back.dto.subscribe.SubscribeRespDto.SubscribeListRespDto;
 import site.hobbyup.class_final_back.dto.subscribe.SubscribeRespDto.SubscribeSaveRespDto;
 import site.hobbyup.class_final_back.service.SubscribeService;
 
@@ -49,4 +46,14 @@ public class SubscribeApiController {
         return new ResponseEntity<>(new ResponseDto<>("구독 취소", null), HttpStatus.CREATED);
     }
 
+    @GetMapping("api/user/{userId}/subscribe")
+    public ResponseEntity<?> getSubscribeList(@PathVariable Long userId, @AuthenticationPrincipal LoginUser loginUser) {
+        log.debug("디버그 : 구독목록보기 controller");
+        if (userId != loginUser.getUser().getId()) { // 권한체크 - 세션의 id, userid가 같으면 상세보기
+            throw new CustomApiException("권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+        SubscribeListRespDto subscribeListRespDto = subscribeService.getSubscribeList(userId);
+        log.debug("디버그 : " + subscribeListRespDto.getSubscribes().get(0).getLesson().getName());
+        return new ResponseEntity<>(new ResponseDto<>("구독리스트보기", subscribeListRespDto), HttpStatus.OK);
+    }
 }
