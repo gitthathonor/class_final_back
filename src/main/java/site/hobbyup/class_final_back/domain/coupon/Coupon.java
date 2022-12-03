e46c8a2e4f122e7b8701236691924e52479ace84
hyonna12 <hyonna12@gmail.com>
2022-12-04 03:33:50 +0900

    coupon entity, repository 생성

--- src/main/java/site/hobbyup/class_final_back/domain/coupon/Coupon.java
+++ src/main/java/site/hobbyup/class_final_back/domain/coupon/Coupon.java
@@ -1,6 +1,12 @@
 package site.hobbyup.class_final_back.domain.coupon;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AccessLevel;
@@ -14,7 +20,14 @@ import lombok.ToString;
 @Table(name = "coupon")
 @Entity
 public class Coupon {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 50)
+    private String title;
+    @Column(nullable = false, length = 20)
+    private Long price;
+    private Timestamp expiredDate;
+    private Timestamp createdAt;
 
 }
---
+++ src/main/java/site/hobbyup/class_final_back/domain/coupon/CouponRepository.java
@@ -0,0 +1,7 @@
+package site.hobbyup.class_final_back.domain.coupon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+}
