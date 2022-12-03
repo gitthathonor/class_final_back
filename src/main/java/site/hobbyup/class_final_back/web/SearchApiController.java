0ddf9204eb1587de416187f8ce6fb8ac1ac8f761
hyonna12 <hyonna12@gmail.com>
2022-12-04 03:17:19 +0900

    검색하기 기능 완료

--- src/main/java/site/hobbyup/class_final_back/web/SearchApiController.java
+++ src/main/java/site/hobbyup/class_final_back/web/SearchApiController.java
@@ -2,12 +2,14 @@ package site.hobbyup.class_final_back.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import site.hobbyup.class_final_back.service.LessonService;
+import site.hobbyup.class_final_back.dto.search.SearchRespDto.SearchListRespDto;
 import site.hobbyup.class_final_back.service.SearchService;
 
 @RequiredArgsConstructor
@@ -16,9 +18,11 @@ public class SearchApiController {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final SearchService searchService;
 
-    @GetMapping("/api/class")
-    public String searchClass(@RequestParam(value = "keyword") String keyword) {
-        searchService.searchClass(keyword);
-        return "";
+    // 인증 필요하게 할건지(로그인여부확인)
+    @GetMapping("/api/search")
+    public ResponseEntity<?> searchClass(@RequestParam(value = "keyword") String keyword) {
+        SearchListRespDto searchListRespDto = searchService.searchClass(keyword);
+        return new ResponseEntity<>(searchListRespDto, HttpStatus.CREATED);
     }
+
 }
