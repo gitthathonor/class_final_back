19ec5b9d0242a55a82381be4783fceb41d313789
gitthathonor <95184357+gitthathonor@users.noreply.github.com>
2022-12-04 16:30:27 +0900

    [merge] lesson-detail 진행중
    * 관심사 엔티티 및 리포지토리 생성
    
    * 회원가입에서 카테고리 넣기 dto, service 작성완료
    
    * 현재 findAllByUserId 쿼리에서 오류 터지는 중
    
    * 회원가입 시, 관심사도 같이 테이블에 삽입하기 테스트 코드까지 완료
    
    * 클래스 카테고리별 리스트 - 예산 필터링 적용완료
    
    * table생성 sql 추가시켜놓음
    
    * readme파일 생성
    
    * lesson-review join해서 ResponseDto 작성중
    
    * lesson 삭제 시 user와의 관계 때문에 영속성 전리 문제로 인해서 CascadeType.DETACH 로 변경
    
    * lesson-detail ResponseDto 작성하다 중단

--- src/main/java/site/hobbyup/class_final_back/domain/interest/Interest.java
+++ src/main/java/site/hobbyup/class_final_back/domain/interest/Interest.java
@@ -1,13 +1,10 @@
 package site.hobbyup.class_final_back.domain.interest;
 
-import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
