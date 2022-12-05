33a8de46a269f81699f617ed41cc4e24cd4ef9e1
gitthathonor <jeong.suyoung92@gmail.com>
2022-12-05 11:24:05 +0900

    결제와 결제타입 엔티티 및 리포지토리 생성

---
+++ src/main/java/site/hobbyup/class_final_back/domain/payment/PaymentRepository.java
@@ -0,0 +1,7 @@
+package site.hobbyup.class_final_back.domain.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
