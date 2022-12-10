3a19b9ddb421f927841f1da2d95529b9415ce237
gitthathonor <jeong.suyoung92@gmail.com>
2022-12-10 16:09:04 +0900

    로그인 시 jwt에 담는 데이터에 role도 포함

--- src/main/java/site/hobbyup/class_final_back/dto/user/UserRespDto.java
+++ src/main/java/site/hobbyup/class_final_back/dto/user/UserRespDto.java
@@ -50,11 +50,13 @@ public class UserRespDto {
     public static class LoginRespDto {
         private Long id;
         private String username;
+        private String role;
         private String createdAt;
 
         public LoginRespDto(User user) {
             this.id = user.getId();
             this.username = user.getUsername();
+            this.role = user.getRole().getValue();
             this.createdAt = user.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         }
     }
