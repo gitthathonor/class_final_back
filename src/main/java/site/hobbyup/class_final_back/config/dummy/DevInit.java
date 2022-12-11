ee91051eaebaa1d9efce83895c0cdc7fd65f4b30
gitthathonor <jeong.suyoung92@gmail.com>
2022-12-11 17:51:01 +0900

    DevInit, Coupon 파일 수정

--- src/main/java/site/hobbyup/class_final_back/config/dummy/DevInit.java
+++ src/main/java/site/hobbyup/class_final_back/config/dummy/DevInit.java
@@ -11,6 +11,10 @@ import site.hobbyup.class_final_back.domain.category.CategoryRepository;
 import site.hobbyup.class_final_back.domain.interest.InterestRepository;
 import site.hobbyup.class_final_back.domain.lesson.Lesson;
 import site.hobbyup.class_final_back.domain.lesson.LessonRepository;
+import site.hobbyup.class_final_back.domain.review.Review;
+import site.hobbyup.class_final_back.domain.review.ReviewRepository;
+import site.hobbyup.class_final_back.domain.subscribe.Subscribe;
+import site.hobbyup.class_final_back.domain.subscribe.SubscribeRepository;
 import site.hobbyup.class_final_back.domain.user.User;
 import site.hobbyup.class_final_back.domain.user.UserRepository;
 
@@ -21,7 +25,8 @@ public class DevInit extends DummyEntity {
     @Profile("dev")
     @Bean
     public CommandLineRunner dataSetting(UserRepository userRepository, CategoryRepository categoryRepository,
-            InterestRepository interestRepository, LessonRepository lessonRepository) {
+            InterestRepository interestRepository, LessonRepository lessonRepository, ReviewRepository reviewRepository,
+            SubscribeRepository subscribeRepository) {
 
         return (args) -> {
             User ssar = userRepository.save(newUser("ssar"));
@@ -36,7 +41,7 @@ public class DevInit extends DummyEntity {
             Category game = categoryRepository.save(newCategory("게임"));
             Category others = categoryRepository.save(newCategory("기타"));
 
-            Lesson lesson = lessonRepository.save(newLesson("더미1", 10000L, ssar, beauty));
+            Lesson lesson1 = lessonRepository.save(newLesson("더미1", 10000L, ssar, beauty));
             Lesson lesson2 = lessonRepository.save(newLesson("더미2", 20000L, ssar, sports));
             Lesson lesson3 = lessonRepository.save(newLesson("더미3", 50000L, ssar, music));
             Lesson lesson4 = lessonRepository.save(newLesson("더미4", 34500L, cos, music));
@@ -46,6 +51,20 @@ public class DevInit extends DummyEntity {
             Lesson lesson8 = lessonRepository.save(newLesson("더미8", 40000L, ssar, sports));
             Lesson lesson9 = lessonRepository.save(newLesson("더미9", 50000L, ssar, sports));
             Lesson lesson10 = lessonRepository.save(newLesson("더미10", 70000L, ssar, sports));
+
+            Review review1 = reviewRepository.save(newReivew("너무 좋은 강의입니다.", 4.5, ssar, lesson1));
+            Review review2 = reviewRepository.save(newReivew("생각했던 것보다 더 좋네요!", 4.0, cos, lesson1));
+            Review review3 = reviewRepository.save(newReivew("별로네요", 3.0, ssar, lesson2));
+            Review review4 = reviewRepository.save(newReivew("도대체 이 강의 하시는 이유가 뭐죠?", 2.5, ssar, lesson3));
+            Review review5 = reviewRepository.save(newReivew("피곤하다", 2.0, cos, lesson2));
+            Review review6 = reviewRepository.save(newReivew("이정도 퀄리티면 좋은 거 같아요, 다만 목소리가...", 3.5, cos, lesson8));
+            Review review7 = reviewRepository.save(newReivew("토큰 없음", 1.5, cos, lesson7));
+
+            Subscribe subscribe1 = subscribeRepository.save(newSubscribe(ssar, lesson1));
+            Subscribe subscribe2 = subscribeRepository.save(newSubscribe(ssar, lesson2));
+            Subscribe subscribe3 = subscribeRepository.save(newSubscribe(cos, lesson3));
+            Subscribe subscribe4 = subscribeRepository.save(newSubscribe(cos, lesson8));
+            Subscribe subscribe5 = subscribeRepository.save(newSubscribe(cos, lesson9));
 
         };
 
