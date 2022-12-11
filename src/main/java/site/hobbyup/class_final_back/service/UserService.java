9a8a4fec5fcf6f06b158e6f1c50ffbab8246e780
gitthathonor <jeong.suyoung92@gmail.com>
2022-12-11 13:58:04 +0900

    회원가입 시 카테고리가 null일 때, 처리하기 코드 추가

--- src/main/java/site/hobbyup/class_final_back/service/UserService.java
+++ src/main/java/site/hobbyup/class_final_back/service/UserService.java
@@ -68,6 +68,10 @@ public class UserService {
 
         // 3. 관심사 저장
         log.debug("디버그 : JoinReqDto가 선택한 카테고리들 : " + joinReqDto.getCategoryIds());
+
+        if (joinReqDto.getCategoryIds() == null) {
+            return new JoinRespDto(userPS);
+        }
         List<Category> categoryListPS = categoryRepository.findAllById(joinReqDto.getCategoryIds());
 
         for (Category category : categoryListPS) {
@@ -83,7 +87,7 @@ public class UserService {
         Coupon coupon = Coupon.builder().title("회원가입 쿠폰").price(10000L).expiredDate("2022-12-22").user(userPS).build();
         couponRepository.save(coupon);
 
-        // 4. DTO 응답
+        // 6. DTO 응답
         return new JoinRespDto(userPS, interestListPS);
     }
 
